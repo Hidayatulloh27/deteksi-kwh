321b29ce376c69e10ba9ab5cc42e9769eb9ecade
Hidayat Tullah <ah3577470@gmail.com>
2026-05-30 07:40:06 +0700

    fix firebase key path

--- server/aplikasi.py
+++ server/aplikasi.py
@@ -20,8 +20,10 @@ app = Flask(
     
 )
 CORS(app)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 cred = credentials.Certificate(
-    "firebase-adminsdk.json"
+    os.path.join(BASE_DIR, "..", "firebase-key.json")
 )
 
 if not firebase_admin._apps:
